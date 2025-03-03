ae90bdb7eec0d7fe61345d26b170b704d96bc43a
Gayathri-Raj-18 <Gayathri.Anandaraj@cognizant.com>
2025-03-03 16:14:54 +0530

    Add files via upload

---
+++ HealthCareP/Notification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Notification2.Models; // Ensure this namespace is correct
+
+namespace Notification2.Models
+{
+    public class Notification
+    {
+        [Key]
+        public int NotificationID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        [Required]
+        public DateTime SentDate { get; set; }
+
+        public bool IsRead { get; set; }
+
+        [ForeignKey(nameof(UserID))]
+        public virtual User User { get; set; }
+    }
+}
